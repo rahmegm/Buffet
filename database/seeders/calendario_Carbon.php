9c3f49ddddbc5814d92fff73f412f8007194c1ce
Gustavo Morandi Rahmé <gmorandirahme@gmail.com>
2023-10-20 16:14:52 -0300

    calendario_Carbon seeder
    tentando fazer a seeder do calendario

---
+++ database/seeders/calendario_Carbon.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class calendario_Carbon extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+      //inserindo dados na tabela calendario
+    //   DB::table('calendario')->insert([
+    //     'data' => Str::random(10),
+    //     'status' => Str::random(10).'@gmail.com'
+    // ]);
+    // !!!!!!terminar de ver!!!!!!!!!!!
+    }
+}
