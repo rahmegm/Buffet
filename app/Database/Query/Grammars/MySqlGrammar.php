8b06dd71acab74180c85239698b0d2c09ebb1a30
Gustavo Morandi Rahmé <gmorandirahme@gmail.com>
2023-10-20 09:19:55 -0300

    Initial commit

---
+++ app/Database/Query/Grammars/MySqlGrammar.php
@@ -0,0 +1,12 @@
+<?php
+// app/Database/Query/Grammars/MySqlGrammar.php
+
+namespace App\Database\Query\Grammars;
+
+class MySqlGrammar extends \Illuminate\Database\Query\Grammars\MySqlGrammar
+{
+    public function getDateFormat()
+    {
+        return 'd-m-Y H:i:s.u';
+    }
+}
