1978265703f831963ac7f3f1acdb30144828a5f1
Gustavo Campos <gustavoalvarezdecampos@gmail.com>
2023-11-28 09:53:32 -0300

    commit banco de dados
    Commit do banco de dados resolvido pelo professor.

--- database/migrations/2023_11_17_171746_create_birthday_table.php
+++ database/migrations/2023_11_17_171746_create_birthday_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->timestamps();
             $table->string('title');
             $table->text('description');
-            
         });
     }
 
--- database/migrations/2023_11_20_225213_add_date_to_birthdays_table.php
+++ database/migrations/2023_11_20_225213_add_date_to_birthdays_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('birthdays', function (Blueprint $table) {
+        Schema::table('birthday', function (Blueprint $table) {
             $table->dateTime('date');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('birthdays', function (Blueprint $table) {
+        Schema::table('birthday', function (Blueprint $table) {
             $table->dropColumn('date');
         });
     }
